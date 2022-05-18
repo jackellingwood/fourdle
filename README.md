c6ed2088291a2b52809906def14c04cce6d101f9
jackellingwood <jack.ellingwood@baxter-academy.org>
2022-05-18 13:30:32 -0400

    finished structure

--- README.md
+++ README.md
@@ -3,14 +3,15 @@ I'm gonna make wordle but it's only four letters using good ol' python.
 
 Goals:
 * Find dictionary library for words
-* Color text appropriately
-* Create game structure, looping
+* Color text appropriately X
+* Create game structure, looping X
 
 Stretch:
 * Log past scores to save file
 * Make it look pretty
 * Filter swears/slurs
 
-Journal
-5/12/22: Started repo, messed around with color codes with gusto.
-5/16/22: Added some words from https://7esl.com/4-letter-words/. Will add more later.
+Journal:
+* 5/12/22: Started repo, messed around with color codes with gusto.
+* 5/16/22: Added some words from https://7esl.com/4-letter-words/. Will add more later.
+* 5/18/22: Finished game structure, with a few bugs.
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+
 words = {
     # list adapted from https://7esl.com/4-letter-words/
     'AREA',
@@ -338,23 +340,89 @@ colors = {
     'cyan': '\u001b[36m',
     'white': '\u001b[37m',
     'none': '\u001b[0m'
-};
+}
+
+class Letter:
+    def __init__(self, name, pos, color='none'):
+        self.name = name
+        self.pos = pos
+        self.color = color
+
+    def changeColor(self, newColor):
+        self.color = newColor
+
+    def typeOut(self):
+        return colors[self.color] + self.name + colors['none']
 
 def addColor(s, color):
     return colors[color] + s + colors['none']
 
-letter1 = ''
-letter2 = ''
-letter3 = ''
-letter4 = ''
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {addColor(name, "cyan")}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {addColor(name, "blue")}')  # Press Ctrl+F8 to toggle the breakpoint.
+def printSpaced(inList):
+    for i in inList:
+        print(i)
 
+def dramaticPrint(inList):
+    for i in inList:
+        time.sleep(0.5)
+        print(i.typeOut(), end=' ')
+    time.sleep(2)
+    print()
+
+def main():
+    while True:
+        toGuess = 'MAYO'
+        unfinished = True
+        tries = 5
+        theBoard = []
+        tryNum = 0
+        for c in range(tries):
+            theBoard.append('- - - -')
+        while unfinished:
+            if tryNum != tries:
+                tryNum += 1
+                letterCheck = True
+                while letterCheck:
+                    guessedStr = input('>>')
+                    isLetter = True
+                    for l in guessedStr:
+                        if not l.isalpha():
+                            isLetter = False
+                    if isLetter and len(guessedStr) == 4:
+                        letterCheck = False
+                    else:
+                        print('Try a four-letter word.')
+                letter1 = Letter(guessedStr[0].capitalize(), 1)
+                letter2 = Letter(guessedStr[1].capitalize(), 2)
+                letter3 = Letter(guessedStr[2].capitalize(), 3)
+                letter4 = Letter(guessedStr[3].capitalize(), 4)
+                letterList = [letter1, letter2, letter3, letter4]
+                for i in letterList:
+                    if i.name == toGuess[i.pos-1]:
+                        i.changeColor('green')
+                    elif i.name in toGuess:
+                        i.changeColor('yellow')
+                    else:
+                        i.changeColor('none')
+                dramaticPrint(letterList)
+                print('= = = =')
+                theBoard[tryNum-1] = f'{letter1.typeOut()} {letter2.typeOut()} {letter3.typeOut()} {letter4.typeOut()}'
+                printSpaced(theBoard)
+                print('= = = =')
+                if f'{letter1.name}{letter2.name}{letter3.name}{letter4.name}' == toGuess:
+                    print(f'You won in {tryNum} guess(es)!')
+                    time.sleep(3)
+                    print('Starting a new game...')
+                    time.sleep(3)
+                    unfinished = False
+            else:
+                print(f"You're out of guesses! The word was {toGuess}.")
+                time.sleep(3)
+                print('Starting a new game...')
+                time.sleep(3)
+                unfinished = False
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
