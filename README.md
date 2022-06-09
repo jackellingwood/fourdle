0176dd7f25a4eeffab5159e1aa3bca897892d533
jackellingwood <jack.ellingwood@baxter-academy.org>
2022-06-09 14:07:23 -0400

    did everything, final, hopefully

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 I'm gonna make wordle but it's only four letters using good ol' python.
 
 Goals:
-* Find dictionary library for words
+* Find dictionary library for words X
 * Color text appropriately X
 * Create game structure, looping X
 
 Stretch:
-* Log past scores to save file
-* Make it look pretty
-* Filter swears/slurs
+* Log past scores to save file X
+* Make it look pretty - Not gonna happen
+* Filter swears/slurs - Also not gonna happen, they should count but aren't in the game words
 
 Journal:
 * 5/12/22: Started repo, messed around with color codes with gusto.
 * 5/16/22: Added some words from https://7esl.com/4-letter-words/. Will add more later.
 * 5/18/22: Finished game structure, with a few bugs.
+* I kinda missed journaling for a while, here's some stuff that happened in order.
+  * Added stats.
+  * Finished structure.
+  * Changed coloring to fit Wordle logic.
+  * Fixed bugs.
+  * Commented code
+* 6/9/22: Finally fixed the last bug, a nasty one. Final version.
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# from rich import print
 import time
+import random
+import pickle
+from english_words import english_words_lower_alpha_set as ewords
+import termplotlib as tpl
 
 words = {
+    # puzzle words
     # list adapted from https://7esl.com/4-letter-words/
     'AREA',
     'ARMY',
@@ -331,6 +333,7 @@ words = {
 
 }
 colors = {
+    # ansi color escape codes
     'black': '\u001b[30m',
     'red': '\u001b[31m',
     'green': '\u001b[32m',
@@ -342,7 +345,19 @@ colors = {
     'none': '\u001b[0m'
 }
 
-class Letter:
+
+def flush_input(): # Code from https://stackoverflow.com/questions/2520893/how-to-flush-the-input-stream
+    #stops players from typing during printing
+    try:
+        import msvcrt
+        print('msvcrt')
+        while msvcrt.kbhit():
+            msvcrt.getch()
+    except ImportError:
+        import sys, termios    #for linux/unix
+        termios.tcflush(sys.stdin, termios.TCIOFLUSH)
+
+class Letter: #represents a character
     def __init__(self, name, pos, color='none'):
         self.name = name
         self.pos = pos
@@ -351,17 +366,25 @@ class Letter:
     def changeColor(self, newColor):
         self.color = newColor
 
-    def typeOut(self):
+    def typeOut(self): # returns the name sandwiched in color codes
         return colors[self.color] + self.name + colors['none']
 
-def addColor(s, color):
-    return colors[color] + s + colors['none']
+class SaveFile: # object that gets pickled
+    def __init__(self, played, wins, curr_streak, max_streak, distribution):
+        self.played = played
+        self.wins = wins
+        self.curr_streak = curr_streak
+        self.max_streak = max_streak
+        self.distribution = distribution
 
-def printSpaced(inList):
+# def addColor(s, color): #Unused
+#     return colors[color] + s + colors['none']
+
+def printSpaced(inList): #for printing the board
     for i in inList:
         print(i)
 
-def dramaticPrint(inList):
+def dramaticPrint(inList): # for printing the word dramatically
     for i in inList:
         time.sleep(0.5)
         print(i.typeOut(), end=' ')
@@ -369,60 +392,159 @@ def dramaticPrint(inList):
     print()
 
 def main():
-    while True:
-        toGuess = 'MAYO'
+    while True: #setup code, runs before game
+        toGuess = random.choice(list(words))
         unfinished = True
         tries = 5
         theBoard = []
         tryNum = 0
-        for c in range(tries):
-            theBoard.append('- - - -')
-        while unfinished:
-            if tryNum != tries:
+        for c in range(tries): #make blank spaces, scales with tries
+            theBoard.append('|- - - -|')
+        while unfinished: #runs until solved or out of tries
+            if tryNum != tries:# try counter
                 tryNum += 1
                 letterCheck = True
-                while letterCheck:
+                while letterCheck: #asks for input and checks for validity
                     guessedStr = input('>>')
-                    isLetter = True
-                    for l in guessedStr:
-                        if not l.isalpha():
-                            isLetter = False
-                    if isLetter and len(guessedStr) == 4:
-                        letterCheck = False
+                    isLetters = True
+                    appearances = dict()
+                    for a in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+                        appearances[a] = toGuess.count(a)
+                    letterList = []
+                    finalList = [None, None, None, None]
+                    if len(guessedStr) == 4:
+                        for strNum in range(len(guessedStr)):
+                            if not guessedStr[strNum].capitalize().isalpha():
+                                isLetters = False
+                            letterToCreate = Letter(guessedStr[strNum].capitalize(), strNum)
+                            letterList.append(letterToCreate)
+                        for l1 in letterList:
+                            letterToColor = l1
+                            if letterToColor.name == toGuess[letterToColor.pos]:
+                                letterToColor.changeColor('green')
+                                appearances[letterToColor.name] -= 1
+                                finalList[letterToColor.pos] = letterToColor
+                        for l2 in letterList:
+                            letterToColor = l2
+                            if not letterToColor.color == 'green':
+                                if letterToColor.name in toGuess and appearances[letterToColor.name] > 0:
+                                    letterToColor.changeColor('yellow')
+                                    appearances[letterToColor.name] -= 1
+                                    finalList[letterToColor.pos] = letterToColor
+                                else:
+                                    letterToColor.changeColor('none')
+                                    finalList[letterToColor.pos] = letterToColor
+                        if isLetters:
+                            if guessedStr.upper() == toGuess or guessedStr.lower() in ewords:
+                                letterCheck = False
+                            else:
+                                print('Try a four-letter word.')
+                        else:
+                            print('Try a four-letter word.')
                     else:
                         print('Try a four-letter word.')
-                letter1 = Letter(guessedStr[0].capitalize(), 1)
-                letter2 = Letter(guessedStr[1].capitalize(), 2)
-                letter3 = Letter(guessedStr[2].capitalize(), 3)
-                letter4 = Letter(guessedStr[3].capitalize(), 4)
-                letterList = [letter1, letter2, letter3, letter4]
-                for i in letterList:
-                    if i.name == toGuess[i.pos-1]:
-                        i.changeColor('green')
-                    elif i.name in toGuess:
-                        i.changeColor('yellow')
-                    else:
-                        i.changeColor('none')
-                dramaticPrint(letterList)
-                print('= = = =')
-                theBoard[tryNum-1] = f'{letter1.typeOut()} {letter2.typeOut()} {letter3.typeOut()} {letter4.typeOut()}'
+                # letter1 = Letter(guessedStr[0].capitalize(), 1) # creates letters out of input
+                # letter2 = Letter(guessedStr[1].capitalize(), 2)
+                # letter3 = Letter(guessedStr[2].capitalize(), 3)
+                # letter4 = Letter(guessedStr[3].capitalize(), 4)
+                # finalList = [letter1, letter2, letter3, letter4]
+                # for i in finalList: #colors letters based on puzzle word
+                #     if i.name == toGuess[i.pos-1]:
+                #         i.changeColor('green')
+                #     elif i.name in toGuess:
+                #         i.changeColor('yellow')
+                #     else:
+                #         i.changeColor('none')
+                dramaticPrint(finalList) #big reveal
+                print('|=+=+=+=|') #border for fanciness
+                theBoard[tryNum-1] = f'|{finalList[0].typeOut()} {finalList[1].typeOut()} {finalList[2].typeOut()} {finalList[3].typeOut()}|'
                 printSpaced(theBoard)
-                print('= = = =')
-                if f'{letter1.name}{letter2.name}{letter3.name}{letter4.name}' == toGuess:
+                print('|=+=+=+=|') #border for fanciness
+                if f'{finalList[0].name}{finalList[1].name}{finalList[2].name}{finalList[3].name}' == toGuess: # win state
+                    try: # if the save exists, read it in
+                        with open('save.pkl', 'rb') as f:
+                            save = pickle.load(f)
+                    except: # if it doesn't, make a save
+                        with open('save.pkl', 'wb') as f:
+                            save = SaveFile(0, 0, 0, 0, [0, 0, 0, 0, 0])
+                            pickle.dump(save, f)
+                    #change save attributes
+                    newPlayed = save.played+1
+                    newWins = save.wins+1
+                    newCurrStreak = save.curr_streak+1
+                    if save.curr_streak+1 > save.max_streak:
+                        newMax = save.curr_streak+1
+                    else:
+                        newMax = save.max_streak
+                    newDist = save.distribution
+                    newDist[tryNum-1] += 1
+                    with open('save.pkl', 'wb') as f: #save the file
+                        pickle.dump(SaveFile(newPlayed, newWins, newCurrStreak, newMax, newDist), f)
                     print(f'You won in {tryNum} guess(es)!')
                     time.sleep(3)
+                    print('---Stats---')
+                    print(f'Played: {newPlayed}, Win %: {round((newWins / newPlayed) * 100, 2)},')
+                    print(f'Current Streak: {newCurrStreak}, Max Streak: {newMax}')
+                    print('---Guess Distribution---')
+                    fig = tpl.figure() # bar graph
+                    fig.barh(newDist, ["1", "2", "3", "4", "5"], force_ascii=True)
+                    fig.show()
+                    another = input('Play another? [Y/N] : ') # asks if you wanna play again
+                    if another == 'Y' or another == 'y':
+                        print('Starting a new game...')
+                        time.sleep(3)
+                        unfinished = False
+                    elif another == 'N' or another == 'n':
+                        print('Okay, see you later, champ.')
+                        exit()
+                    else:
+                        print("I don't know what that means so I'll take it as a yes.")
+                        print('Starting a new game...')
+                        time.sleep(3)
+                        unfinished = False
+            else:
+                try:
+                    with open('save.pkl', 'rb') as f:
+                        save = pickle.load(f)
+                except:
+                    with open('save.pkl', 'wb') as f:
+                        save = SaveFile(0, 0, 0, 0, [0, 0, 0, 0, 0])
+                        pickle.dump(save, f)
+                newPlayed = save.played+1
+                newWins = save.wins
+                newCurrStreak = 0
+                newMax = save.max_streak
+                newDist = save.distribution
+                with open('save.pkl', 'wb') as f:
+                    pickle.dump(SaveFile(newPlayed, newWins, newCurrStreak, newMax, newDist), f)
+                print(f"You're out of guesses! The word was {toGuess}.")
+                time.sleep(3)
+                print('---Stats---')
+                print(f'Played: {newPlayed}, Win %: {round((newWins / newPlayed) * 100, 2)},')
+                print(f'Current Streak: {newCurrStreak}, Max Streak: {newMax}')
+                print('---Guess Distribution---')
+                fig = tpl.figure()
+                fig.barh(newDist, ["1", "2", "3", "4", "5"], force_ascii=True)
+                fig.show()
+                another = input('Play another? [Y/N] : ')
+                if another == 'Y' or another == 'y':
                     print('Starting a new game...')
                     time.sleep(3)
                     unfinished = False
-            else:
-                print(f"You're out of guesses! The word was {toGuess}.")
-                time.sleep(3)
-                print('Starting a new game...')
-                time.sleep(3)
-                unfinished = False
+                elif another == 'N' or another == 'n':
+                    print('Okay, quitter.')
+                    exit()
+                else:
+                    print("I don't know what that means so I'll take it as a yes.")
+                    print('Starting a new game...')
+                    time.sleep(3)
+                    unfinished = False
 
-# Press the green button in the gutter to run the script.
+def test():
+    while True:
+        flush_input()
+        time.sleep(1)
+        input('test')
+
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
